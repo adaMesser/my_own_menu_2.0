36a4f1e1ad756a33172de0953f579d328094469a
ada_messer <ladydowaakhin@gmail.com>
2016-06-30 19:34:34 +0300

    invoke_all, drupal_alter

--- my_menu.api.php
+++ my_menu.api.php
@@ -4,6 +4,10 @@
 /**
  * hook my_menu menu
  */
-function hook_my_menu_menu() {
+function hook_my_menu_menu($route, $item) {
 
+}
+
+function hook_my_menu_menu_alter(&$subitems, $route, $item) {
+  
 }
